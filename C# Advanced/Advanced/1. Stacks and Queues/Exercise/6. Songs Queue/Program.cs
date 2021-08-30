1ffb54b4f47cf597decd7e5adb86ff94bb6bb2e1
Iveta Toneva <iveta.v.toneva@gmail.com>
2021-08-30 11:45:55 +0300

    Stacks and Queues - Exercise
    Stacks and Queues - Exercise

---
+++ C# Advanced/Advanced/1. Stacks and Queues/Exercise/6. Songs Queue/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Queue<string> queue = new Queue<string>();
+            for (int i = 0; i < songs.Length; i++)
+            {
+                queue.Enqueue(songs[i]);
+            }
+            string command = Console.ReadLine();
+            while (queue.Count>0)
+            {
+                if (command.Contains("Play"))
+                {
+                    queue.Dequeue();
+                }
+                else if (command.Contains("Add"))
+                {
+                    string[] splitted = command.Split("Add ");
+                    string song = splitted[1];
+                    if (queue.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        queue.Enqueue(song);
+                    }
+
+                }
+                else if (command.Contains("Show"))
+                {
+                    Console.WriteLine(string.Join(", ",queue));
+                }
+
+
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("No more songs!");
+
+        }
+    }
+}
---
+++ C# Advanced/Advanced/1. Stacks and Queues/Exercise/7. Truck Tour/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Truck_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pumps = int.Parse(Console.ReadLine());
+
+            Queue<string> circle = new Queue<string>();
+
+            for (int i = 0; i < pumps; i++)
+            {
+                string input = Console.ReadLine();
+
+                input += $" {i}"; 
+
+                circle.Enqueue(input);
+            }
+
+            int totalFuel = 0;
+
+            for (int i = 0; i < pumps; i++)
+            {
+                string currentInfo = circle.Dequeue();
+
+                int[] info = currentInfo.Split().Select(int.Parse).ToArray();
+
+                int fuel = info[0];
+                int distance = info[1];
+
+                totalFuel += fuel;
+
+                if (totalFuel >= distance)
+                {
+                    totalFuel -= distance;
+                }
+                else
+                {
+                    totalFuel = 0;
+                    i = -1;
+                }
+
+                circle.Enqueue(currentInfo);
+
+            }
+
+            string[] firstElement = circle.Dequeue().Split().ToArray();
+
+            Console.WriteLine(firstElement[2]);
+        }
+    }
+}

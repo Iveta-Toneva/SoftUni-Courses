c9a05685c2e2219320be4886cfb207df5d3ce898
Iveta Toneva <iveta.v.toneva@gmail.com>
2021-09-25 15:28:15 +0300

    Advanced
    Advanced

---
+++ C# Advanced/Advanced/6.Generics/Lab/2. Array Creator/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[]array=(ArrayCreator<int>.Create(4,44));
+            Console.WriteLine(string.Join(" ",array));
+        }
+    }
+}

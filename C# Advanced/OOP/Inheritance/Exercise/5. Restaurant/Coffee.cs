7c87f85ad676e87764e1253f19682df556351a96
Iveta Toneva <iveta.v.toneva@gmail.com>
2021-11-02 20:32:13 +0200

    OOP
    OOP

---
+++ C# Advanced/OOP/Inheritance/Exercise/5. Restaurant/Coffee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const decimal CaffePrice = 3.50m;
+        private const double CaffeMilliliters = 50;
+        public Coffee(string name, double caffeine) : base(name, CaffePrice, CaffeMilliliters)
+        {
+           
+            this.Caffeine = caffeine;
+        }
+        public double Caffeine  { get; set; }
+    }
+}
---
+++ C# Advanced/OOP/Inheritance/Exercise/6. Animals/StartUp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> list = new List<Animal>();
+            string type = Console.ReadLine();
+            while (type!= "Beast!")
+            {
+
+                string[] animalInfo = Console.ReadLine().Split();
+                string name = animalInfo[0];
+                int age = int.Parse(animalInfo[1]);
+                string gender = animalInfo[2];
+                if (age<0)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+                if (type=="Cat")
+                {
+                    Cat cat = new Cat(name, age, gender);
+                    list.Add(cat);
+                }
+               else  if (type == "Dog")
+                {
+                    Dog dog = new Dog(name, age, gender);
+                    list.Add(dog);
+                }
+                else if (type == "Frog")
+                {
+                    Frog frog = new Frog(name, age, gender);
+                    list.Add(frog);
+                }
+                else if (type=="Kitten")
+                {
+                    Kitten kitten = new Kitten(name, age);
+                    list.Add(kitten);
+                }
+                else if (type == "Tomcat")
+                {
+                   Tomcat tom = new Tomcat(name, age);
+                    list.Add(tom);
+
+                }
+
+
+
+
+
+
+                type = Console.ReadLine();
+            }
+            foreach (var animal in list)
+            {   
+                Console.WriteLine(animal.GetType().Name);
+                Console.WriteLine($"{animal.Name} {animal.Age} {animal.Gender}");
+                Console.WriteLine(animal.ProduceSound());
+            }
+        }
+    }
+}

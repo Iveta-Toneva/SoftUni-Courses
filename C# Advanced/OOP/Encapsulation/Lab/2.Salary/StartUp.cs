695f37c70f6b4edcbd3f877d4dcc9e1f36c2a2b9
Iveta Toneva <iveta.v.toneva@gmail.com>
2022-03-04 19:14:17 +0200

    encapsulationLab
    encapsulationLab

---
+++ C# Advanced/OOP/Encapsulation/Lab/2.Salary/StartUp.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+  public  class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split();
+                Person person = new Person(line[0],line[1],int.Parse(line[2]));
+                people.Add(person);
+              
+            }
+           var sortedPeople = people.OrderBy(x => x.FirstName).ThenBy(x => x.Age).ToList();
+            Console.WriteLine(string.Join(Environment.NewLine,sortedPeople));
+        }
+    }
+}
---
+++ C# Advanced/OOP/Encapsulation/Lab/3.Validation/StartUp.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+  public  class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split();
+                Person person = new Person(line[0],line[1],int.Parse(line[2]),decimal.Parse(line[3]));
+                people.Add(person);
+              
+            }
+            decimal percent = decimal.Parse(Console.ReadLine());
+            foreach (var person in people)
+            {
+                person.IncreaseSalary(percent);
+                Console.WriteLine(person.ToString());
+            }
+           
+        }
+    }
+}
